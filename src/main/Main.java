f2e1a28fd5edfb5a270ffb7ef654d384ea4c13c4
Back-end developer <danielkremes05@gmail.com>
2024-06-02 15:43:54 -0300

    fix method void play

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,6 +14,7 @@ public class Main {
         iphoneServices.pause();
         System.out.println();
 
+
         // Interface TelephoneHandset
         iphoneServices.call();
         iphoneServices.answer();
